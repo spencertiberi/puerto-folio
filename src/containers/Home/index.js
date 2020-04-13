40dbd39d104458ed7c3d926ab237cc6019dc1e8c
Spencer Tiberi <spencertiberi@gmail.com>
2020-04-13 16:12:40 -0400

    Update home

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,6 +1,5 @@
-import React from 'react'
+import React from "react"
 
-const Home = () => <div>Welcome to the DEV React starter!</div>
-
+const Home = () => <div>Welcome to Puerto Folio!</div>
 
 export default Home
