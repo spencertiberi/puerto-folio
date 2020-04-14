aada924f9bd9aa775671c892ab486b9858df1827
Spencer Tiberi <spencertiberi@gmail.com>
2020-04-14 17:27:58 -0400

    add theme

--- src/theme.js
+++ src/theme.js
@@ -1,8 +1,14 @@
 // Add global styles and fonts here
 
 const theme = {
-  colors: {},
-  fonts: {},
+  colors: {
+    primary: '#bf87ed',
+    secondary: '#fffef7 ',
+    offblack: '#333',
+  },
+  fonts: {
+    main: 'helvetica, arial, sans-sarif',
+  },
 }
 
 export default theme
