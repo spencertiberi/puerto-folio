48f7bfee9bd01de46a12846da5b0d2c64e634d86
Spencer Tiberi <spencertiberi@gmail.com>
2020-04-15 09:11:18 -0400

    Updates

---
+++ precache-manifest.9106d225be8428dc354f04bd29930690.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49eaf3ddb313341131c20383332289cd",
+    "url": "/puerto-folio/index.html"
+  },
+  {
+    "revision": "f6bcb40e554e552db644",
+    "url": "/puerto-folio/static/js/2.3e5aefee.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/puerto-folio/static/js/2.3e5aefee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23e3373d6f719f7664d4",
+    "url": "/puerto-folio/static/js/main.db1dc4cb.chunk.js"
+  },
+  {
+    "revision": "0465c2b0db5f455733e6",
+    "url": "/puerto-folio/static/js/runtime-main.4343afb0.js"
+  }
+]);
