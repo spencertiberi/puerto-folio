3d1207a3d1efeee77814c9efb1eef3a63f99d18b
Spencer Tiberi <spencertiberi@gmail.com>
2020-04-15 09:33:17 -0400

    Add Header to Home page

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import Button from '../../components/Button'
+import Header from '../../components/Header'
 import { Input, Label } from '../../components/Form'
 import PageWrapper from '../../components/PageWrapper'
+import copy from '../../copy'
 
 const Home = () => (
-  <PageWrapper>
-    Welcome to Puerto Folio!
-    <Label>Name</Label>
-    <Input placeholder="Enter Name" />
-    <Button>Submit</Button>
-  </PageWrapper>
+  <fragment>
+    <Header />
+    <PageWrapper>
+      {`Welcome to ${copy.title}!`}
+      <Label>Name</Label>
+      <Input placeholder="Enter Name" />
+      <Button>Submit</Button>
+    </PageWrapper>
+  </fragment>
 )
 
 export default Home
