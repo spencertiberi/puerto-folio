1cdc3cddd160b693f2e79fcac2711976afe9335c
Spencer Tiberi <spencertiberi@gmail.com>
2020-04-18 22:00:29 -0400

    Add general components

--- src/components/PageContainer/index.js
+++ src/components/PageContainer/index.js
@@ -1,15 +1,14 @@
 import styled from 'styled-components'
 
-const PageWrapper = styled.div`
-  padding: 0;
+const PageContainer = styled.div`
+  padding: 15vh 4vw 10vh;
   display: flex;
   flex-direction: column;
   align-items: center;
-  height: 100vh;
-  width: 100vw;
-  overflow: scroll;
+  justify-content: space-between;
+  height: 100%;
   font-family: ${(props) => props.theme.fonts.main};
   color: ${(props) => props.theme.colors.offblack};
 `
 
-export default PageWrapper
+export default PageContainer
