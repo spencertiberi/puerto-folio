c8d5cb46ede97a59b81e05babc914b6abf5ec762
Spencer Tiberi <spencertiberi@gmail.com>
2020-04-18 21:59:44 -0400

    Update router

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import { Switch, Route, BrowserRouter as Router } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 import theme from './theme'
 import Home from './containers/Home'
+import Review from './containers/Review'
+import Name from './containers/Onboarding/Name'
+import Study from './containers/Onboarding/Study'
 
 const App = () => (
   <Router>
     <ThemeProvider theme={theme}>
       <div className="App">
         <Switch>
+          <Route path="/review" component={Review} />
+          <Route path="/study" component={Study} />
+          <Route path="/name" component={Name} />
           <Route path="/" component={Home} />
         </Switch>
       </div>
