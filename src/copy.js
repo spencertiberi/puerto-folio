66002663d943f2ae133c6d4ec904e7a8f2948465
Spencer Tiberi <spencertiberi@gmail.com>
2020-04-18 22:03:28 -0400

    Add copy file

--- src/copy.js
+++ src/copy.js
@@ -2,6 +2,21 @@
 
 const copy = {
   title: 'Puerto Folio',
+  catchPhrase: 'Paint your story',
+  studies: [
+    'Agriculture',
+    'Architecture',
+    'Arts',
+    'Business',
+    'Communications',
+    'Education',
+    'English',
+    'Foreign Language',
+    'Health Sciences',
+    'Philosophy',
+    'Physical Scienes',
+    'Social Sciences',
+  ],
 }
 
 export default copy
