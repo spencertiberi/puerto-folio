2f5edc17e3db2923e3d83d9e4ee2a537795c07d9
Spencer Tiberi <spencertiberi@gmail.com>
2020-04-21 10:17:40 -0400

    Update page title using copy

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Helmet } from 'react-helmet'
 import { Switch, Route, BrowserRouter as Router } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 import theme from './theme'
@@ -6,11 +7,15 @@ import Home from './containers/Home'
 import Review from './containers/Review'
 import Name from './containers/Onboarding/Name'
 import Study from './containers/Onboarding/Study'
+import copy from './copy'
 
 const App = () => (
   <Router>
     <ThemeProvider theme={theme}>
       <div className="App">
+        <Helmet>
+          <title>{copy.title}</title>
+        </Helmet>
         <Switch>
           <Route path="/review" component={Review} />
           <Route path="/study" component={Study} />
