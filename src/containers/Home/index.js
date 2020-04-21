3e0d9fc628f6b4812d32d0d05abe7c9bc3bb9bbe
Spencer Tiberi <spencertiberi@gmail.com>
2020-04-20 21:11:59 -0400

    Update homepage to be responsive

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import PageContainer from '../../components/PageContainer'
 import PageWrapper from '../../components/PageWrapper'
 import Button from '../../components/LinkButton'
 import { Bottom, Image, TextContainer, Title, SubTitle } from './styles'
@@ -9,16 +8,14 @@ import penguin from './assets/penguin.png'
 
 const Home = () => (
   <PageWrapper>
-    <PageContainer>
-      <TextContainer>
-        <Title>{copy.title}</Title>
-        <SubTitle>{copy.catchPhrase}</SubTitle>
-      </TextContainer>
-      <Image src={penguin} />
-      <Button to="/name" width="70vw">
-        GET STARTED
-      </Button>
-    </PageContainer>
+    <TextContainer>
+      <Title>{copy.title}</Title>
+      <SubTitle>{copy.catchPhrase}</SubTitle>
+    </TextContainer>
+    <Image src={penguin} />
+    <Button to="/name" width="300px">
+      GET STARTED
+    </Button>
     <Bottom src={bottom} alt="Bottom of page" />
   </PageWrapper>
 )
--- src/containers/Home/styles.js
+++ src/containers/Home/styles.js
@@ -3,13 +3,20 @@ import styled from 'styled-components'
 export const Bottom = styled.img`
   position: absolute;
   bottom: 0;
-  z-index: -1;
+  z-index: -5;
+  @media only screen and (min-width: 600px) {
+    display: none;
+  }
 `
 
 export const Image = styled.img`
   height: 279px;
   width: auto;
-  padding: 0 0 200px 30px;
+  padding: 0 0 25vh 30px;
+
+  @media only screen and (min-width: 600px) {
+    padding: 0 0 0 30px;
+  }
 `
 
 export const TextContainer = styled.div`
