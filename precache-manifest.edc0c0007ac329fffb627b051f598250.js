e292081d554f2ff78be7d693c8cef9f89848519b
Spencer Tiberi <spencertiberi@gmail.com>
2020-05-01 15:58:58 -0400

    Updates

---
+++ precache-manifest.edc0c0007ac329fffb627b051f598250.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "636397397255c9ae2cf8bff40240e2b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3046b4f0b74587ad6f58",
+    "url": "/static/js/2.dd9bb69f.chunk.js"
+  },
+  {
+    "revision": "3e6933cf6bd44f8f91c7aa725d75c668",
+    "url": "/static/js/2.dd9bb69f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "788f0339a99ae1bc6516",
+    "url": "/static/js/main.adca4ade.chunk.js"
+  },
+  {
+    "revision": "4ff5ab57ebde80b28ebb",
+    "url": "/static/js/runtime-main.a682081a.js"
+  },
+  {
+    "revision": "41aec343661e8c8a37483aa324fe8451",
+    "url": "/static/media/Intersect.41aec343.svg"
+  },
+  {
+    "revision": "dd73980448aceb190806e4a52799e958",
+    "url": "/static/media/littledude.dd739804.png"
+  },
+  {
+    "revision": "3be8a1136018d9194a7d3315939adddd",
+    "url": "/static/media/penguin.3be8a113.png"
+  },
+  {
+    "revision": "3fb6b96ac65b0e1a32a870f0a049ad95",
+    "url": "/static/media/penguinWithClouds.3fb6b96a.png"
+  },
+  {
+    "revision": "f778116e8773a5c4f0ca0a96c30b154a",
+    "url": "/static/media/penguins.f778116e.png"
+  }
+]);
