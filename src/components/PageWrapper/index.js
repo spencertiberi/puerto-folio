8ac222b57b26c6e863f567d933a92132d460f86d
Spencer Tiberi <spencertiberi@gmail.com>
2020-05-02 20:22:21 -0400

    Add custom vh

--- src/components/PageWrapper/index.js
+++ src/components/PageWrapper/index.js
@@ -1,12 +1,13 @@
+import React from 'react'
 import styled from 'styled-components'
 import { Flex } from '@rebass/grid'
 
-const PageWrapper = styled(Flex)`
+const Wrapper = styled(Flex)`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: flex-start;
-  height: 100vh;
+  height: ${(props) => `${props.vh * 100}px`};
   width: 100vw;
   padding: 60px 5vw 0;
   font-family: ${(props) => props.theme.fonts.main};
@@ -14,5 +15,15 @@ const PageWrapper = styled(Flex)`
   background-color: ${(props) =>
     props.dark ? props.theme.colors.primary : 'none'};
 `
+
+const PageWrapper = ({ children, dark }) => {
+  const vh = window.innerHeight * 0.01
+
+  return (
+    <Wrapper vh={vh} dark={dark}>
+      {children}
+    </Wrapper>
+  )
+}
 
 export default PageWrapper
