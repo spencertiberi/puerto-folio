6b775ba2698b1c83ddc6940275227965dacc4454
Spencer Tiberi <spencertiberi@gmail.com>
2020-05-04 19:04:20 -0400

    Add links to portfolio page

--- src/containers/Activities/Landing/Components/PortfolioCard/index.js
+++ src/containers/Activities/Landing/Components/PortfolioCard/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Container, IconCircle, Title } from './styles'
 
-const ActivityCard = ({ title, image, diameter, as, to, onClick }) => (
-  <Container as={as} to={to} onCLick={onClick} diameter={diameter}>
+const ActivityCard = ({ title, image, diameter, as, to, onClick, href }) => (
+  <Container as={as} to={to} onCLick={onClick} diameter={diameter} href={href}>
     <IconCircle src={image} diameter={diameter} />
     <Title>{title}</Title>
   </Container>
--- src/containers/Activities/components/CompletePage/index.js
+++ src/containers/Activities/components/CompletePage/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Button from '../../../../components/LinkButton'
-import { Image, Text, HigherButton } from './styles'
+import { Image, Text } from './styles'
 import PageWrapper from '../../../../components/PageWrapper'
 
 const CompletePage = ({ image, text }) => (
   <PageWrapper dark>
     <Image src={image} />
-    <HigherButton to="/portfolio">CREATE PORTFOLIO</HigherButton>
+    <Button as="a" href="/portfolio" style={{ bottom: '110px' }}>
+      CREATE PORTFOLIO
+    </Button>
     <Text>{text}</Text>
     <Button light to="/">
       TRY ANOTHER ACTIVITY
